6685972f28977db91d23ef0cfc5d3c2e41e15ab6
ThanhToan <codehub.personal@gmail.com>
2024-11-14 16:06:47 +0700

    update Random target

--- Assets/Script/Enemy/Rabbit/RabbitMovement.cs
+++ Assets/Script/Enemy/Rabbit/RabbitMovement.cs
@@ -1,200 +1,95 @@
-﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class RabbitMovement : MonoBehaviour
 {
-    [SerializeField] private float speed = 2f;
-
-    [SerializeField] private float slowSpeed = 2f;
-
-    [SerializeField] private GameObject player;
-
-    [SerializeField] private Transform playerTransform;
-
-    [SerializeField] private GameObject model;
-
-    [SerializeField] private Transform modelTransform;
-
-    [SerializeField] private float detectionRadius = 2f;
-
-    [SerializeField] private float visionAngle = 270f;
-
-    [SerializeField] private Boundary boundary;
-
-    [SerializeField] private float timeSinceLastUpdate = 0f;
-
-    [SerializeField] private Vector2 directionToCenter;
-
     [SerializeField] private ListRabbitVariable rabbits;
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float visionAngle = 270f;
+    public float Velocity { get; private set; }
 
-    [SerializeField] private float moveDuration;
+    private Rigidbody2D rb;
 
-    [SerializeField] private float stopDuration;
-
-    //Vector2 directionToPlayer = Vector2.zero;
-
-    public Vector3 Velocity { private set; get; }
-
-    private Vector2 currentDirection;
-
-    private float stateTimer;
-
-    private bool isMoving = true;
-
-    private void Start()
+    private void Awake()
     {
-        modelTransform = model != null ? transform.Find("Model") : null;
-        playerTransform = player != null ? player.GetComponent<Transform>() : null;
-
-        if (modelTransform == null)
+        rb = GetComponent<Rigidbody2D>();
+        if (rabbits != null && !rabbits.rabbitMovement.Contains(this))
         {
-            Debug.LogError("Model object not found!");
+            rabbits.rabbitMovement.Add(this);
         }
-
-        currentDirection = GetRandomDirection();
-        stateTimer = moveDuration;
-    }
-
-    private void Update()
-    {
-        SetSpriteScale();
     }
 
     private void FixedUpdate()
     {
-        stateTimer -= Time.fixedDeltaTime;
+        Vector2 target = new Vector2(5, 5);
+        MoveTowards(target, speed);
+        RandomTarget();
+    }
 
-        Vector3 nextPosition = transform.position + (Vector3)(currentDirection * speed * Time.fixedDeltaTime);
-        Debug.Log("IsWithinBoundary: " + IsWithinBoundary(nextPosition));
-
-        if (!IsWithinBoundary(nextPosition))
+    private void OnDestroy()
+    {
+        if (rabbits != null)
         {
-            currentDirection = GetDirectionToCenter();
-            Velocity = currentDirection * speed;
-        }
-
-        if (stateTimer <= 0 && isMoving)
-        {
-            isMoving = false;
-            stateTimer = GetRandomTime(); // Reset for stop phase
-            Velocity = Vector3.zero;
-        }
-        else if (stateTimer <= 0 && !isMoving)
-        {
-            isMoving = true;
-            stateTimer = GetRandomTime(); // Reset for move phase
-            currentDirection = GetRandomDirection();
-        }
-
-    }
-
-    private Vector2 GetDirectionToCenter()
-    {
-        if (boundary == null) return Vector2.zero;
-
-        Vector2 center = new Vector2(
-            (boundary.PointA.x + boundary.PointB.x) / 2,
-            (boundary.PointA.y + boundary.PointB.y) / 2
-        );
-        Debug.Log(center);
-        return (center - (Vector2)transform.position).normalized;
-    }
-
-    private bool IsWithinBoundary(Vector3 position)
-    {
-        if (boundary == null) return true;
-        Debug.Log("IsWithinBoundary");
-        return position.x >= boundary.PointA.x && position.x <= boundary.PointB.x &&
-               position.y >= boundary.PointA.y && position.y <= boundary.PointB.y;
-    }
-
-    private float GetRandomTime()
-    {
-        return Random.Range(1f, 3f);
-    }
-
-    private Vector2 GetRandomDirection()
-    {
-        float randomAngle = Random.Range(0f, 2 * Mathf.PI);
-        Vector2 randomDirection = new Vector2(Mathf.Cos(randomAngle), Mathf.Sin(randomAngle)).normalized;
-        return randomDirection == Vector2.zero ? Vector2.up : randomDirection; // Avoid zero vector
-    }
-
-
-    private Vector2 CalculateVelocity()
-    {
-        var rabbitsInRange = RabbitsInRange();
-
-        Vector2 velocity = ((Vector2)transform.forward).normalized * speed;
-
-        return velocity;
-    }
-
-    public void SetSpriteScale()
-    {
-        if (Velocity.x < 0)
-        {
-            modelTransform.localScale = new Vector3(-1, 1, 1);
-        }
-        else if (Velocity.x > 0)
-        {
-            modelTransform.localScale = new Vector3(1, 1, 1);
+            rabbits.rabbitMovement.Remove(this);
         }
     }
 
-    private List<RabbitMovement> RabbitsInRange()
+    public void MoveTowards(Vector2 targetPosition, float moveSpeed)
     {
-        var listRabbit = rabbits.rabbitMovement.FindAll(rabbit => rabbit != this && (rabbit.transform.position - transform.position).magnitude <= detectionRadius && InVisionCone(rabbit.transform.position));
-        return listRabbit;
+        if (rb != null)
+        {
+            Vector2 direction = (targetPosition - rb.position).normalized;
+
+            rb.velocity = direction * moveSpeed;
+        }
     }
 
-    private bool InVisionCone(Vector2 position)
+    public Vector2 RandomTarget()
+
     {
-        Vector2 directionToPosition = position - (Vector2)transform.position;
-        float dotProduct = Vector2.Dot(transform.forward, directionToPosition);
-        float cosHalfVisionAngle = Mathf.Cos(visionAngle * 0.5f * Mathf.Deg2Rad);
-        return dotProduct >= cosHalfVisionAngle;
+        float halfVisionAngle = visionAngle * 0.5f;
+        Vector2 normalizedVelocity = rb.velocity.normalized;
+        Vector2 leftDirection = Quaternion.Euler(0, 0, -halfVisionAngle) * normalizedVelocity;
+        Vector2 rightDirection = Quaternion.Euler(0, 0, halfVisionAngle) * normalizedVelocity;
+
+        float leftPoint = Vector2.Dot(leftDirection, normalizedVelocity);
+        float rightPoint = Vector2.Dot(rightDirection, normalizedVelocity);
+        Debug.Log("Left" + leftPoint + "Right" + rightPoint);
+        float random = Random.Range(leftDirection.x, rightDirection.x);
+        Debug.Log("Random" + random);
+        Vector2 newDirection = new Vector2(random, transform.position.y);
+        Debug.Log(newDirection);
+        return newDirection;
     }
+
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawLine(transform.position, transform.position + (Vector3)Velocity);
-
-        if (boundary != null)
-        {
-            Vector2 topLeft = new(boundary.PointA.x, boundary.PointB.y);
-            Vector2 topRight = boundary.PointB;
-            Vector2 bottomLeft = boundary.PointA;
-            Vector2 bottomRight = new(boundary.PointB.x, boundary.PointA.y);
-
-            Gizmos.color = Color.cyan;
-            Gizmos.DrawLine(topLeft, topRight);
-            Gizmos.DrawLine(topRight, bottomRight);
-            Gizmos.DrawLine(bottomRight, bottomLeft);
-            Gizmos.DrawLine(bottomLeft, topLeft);
-        }
-
-        if (Vector2.Distance(transform.position, playerTransform.position) < detectionRadius)
-        {
-            Gizmos.color = Color.green;
-            Gizmos.DrawLine(transform.position, playerTransform.position);
-        }
-
-        Gizmos.color = Color.red;
-
-        Gizmos.color = Color.red;
-
-        Vector2 normalizedVelocity = Velocity.normalized;
+        Vector2 normalizedVelocity = rb.velocity.normalized;
 
         float halfVisionAngle = visionAngle * 0.5f;
 
         Vector2 leftDirection = Quaternion.Euler(0, 0, -halfVisionAngle) * normalizedVelocity;
         Vector2 rightDirection = Quaternion.Euler(0, 0, halfVisionAngle) * normalizedVelocity;
 
-        Gizmos.DrawLine(transform.position, transform.position + (Vector3)leftDirection * detectionRadius);
-        Gizmos.DrawLine(transform.position, transform.position + (Vector3)rightDirection * detectionRadius);
+        float leftPoint = Vector2.Dot(leftDirection, normalizedVelocity);
+        float rightPoint = Vector2.Dot(rightDirection, normalizedVelocity);
+        Debug.Log("Left" + leftPoint + "Right" + rightPoint);
+        float random = Random.Range(leftDirection.x, rightDirection.x);
+        Vector2 newDirection = new Vector2(random, transform.position.y);
 
-        Gizmos.DrawLine(transform.position, transform.position + (Vector3)normalizedVelocity * detectionRadius);
+        Debug.Log("Random" + random);
+
+        Gizmos.DrawLine(transform.position, transform.position + (Vector3)newDirection * 5f);
+
+
+        Gizmos.DrawLine(new(0, 0, 0), (Vector3)leftDirection);
+
+        Gizmos.DrawLine(transform.position, transform.position + (Vector3)leftDirection * 1f);
+        Gizmos.DrawLine(transform.position, transform.position + (Vector3)rightDirection * 1f);
+
+        Gizmos.DrawLine(transform.position, transform.position + (Vector3)normalizedVelocity * 5f);
+
     }
+
 }
+
