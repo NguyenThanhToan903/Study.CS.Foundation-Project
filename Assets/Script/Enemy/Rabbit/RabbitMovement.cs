ffcd74a10ca639eba599c35d9731f7175e94f444
ThanhToan <codehub.personal@gmail.com>
2024-11-22 22:53:53 +0700

    avoid wall v1

--- Assets/Script/Enemy/Rabbit/RabbitMovement.cs
+++ Assets/Script/Enemy/Rabbit/RabbitMovement.cs
@@ -1,67 +1,148 @@
 ﻿using UnityEngine;
-//old
-public class Racket : MonoBehaviour
+
+public class RabbitMovement : MonoBehaviour
 {
-    [SerializeField] private PlayerInput playerInput;
-    [SerializeField] private PlayerMovement playerMovement;
-    [SerializeField] private ActiveRacket activeRacket;
-    [SerializeField] private Vector3 move;
-    [SerializeField] private Transform racketCollider;
-    [SerializeField] private GameObject pauseGameMenu;
-    private Animator animator;
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float distanceAvoid = 1f;
+    [SerializeField] private float distancePlayerAvoid = 2f;
+    [SerializeField] private float wallDetectionDistance = 3f;
+    [SerializeField] private float sideRayAngle = 40f;
 
-    private void Awake()
-    {
-        animator = GetComponent<Animator>();
-        playerInput = new PlayerInput();
-    }
-
-    private void OnEnable()
-    {
-        playerInput.Enable();
-    }
+    private Vector2 movementDirection;
+    private float waitTimer;
+    private float waitDuration;
+    private bool isWaiting = false;
 
     private void Start()
     {
-        playerInput.Combat.Catch.started += _ => Attack();
+        SetRandomDirection();
     }
 
     private void Update()
     {
-        MouseFollowWithOffset();
-        GetInput();
-    }
+        this.transform.position += (Vector3)(speed * Time.deltaTime * movementDirection);
 
-    private void GetInput()
-    {
-        move = InputManager.Instance.Input.normalized;
-    }
-
-    private void Attack()
-    {
-        animator.SetTrigger("Catching");
-        racketCollider.gameObject.SetActive(true);
-    }
-
-    private void DoneAttackEvent()
-    {
-        racketCollider.gameObject.SetActive(false);
-    }
-
-    private void MouseFollowWithOffset()
-    {
-        if (Time.timeScale == 1f)
+        if (isWaiting)
         {
-            Vector3 mousePos = Input.mousePosition;
-            float temp = 0f;
-            Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerMovement.transform.position);
+            waitTimer += Time.deltaTime;
 
-            float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+            if (waitTimer >= waitDuration)
+            {
+                isWaiting = false;
+                waitTimer = 0f;
 
-            if (move.x < 0) temp = -180f;
-            else temp = 0f;
-            activeRacket.transform.rotation = Quaternion.Euler(0f, temp, angle);
-            racketCollider.transform.rotation = Quaternion.Euler(0f, temp, angle);
+                Debug.Log("Changing direction after waiting...");
+                SetRandomDirection();
+            }
+        }
+        else
+        {
+            AvoidWalls();
+
+            AvoidOtherRabbits();
+        }
+    }
+
+    private void SetRandomDirection()
+    {
+        float randomAngle = Random.Range(0f, 360f);
+        movementDirection = new Vector2(Mathf.Cos(randomAngle), Mathf.Sin(randomAngle)).normalized;
+    }
+
+    private void AvoidWalls()
+    {
+        Vector2 currentPosition = transform.position;
+
+        if (CastRay(currentPosition, movementDirection, wallDetectionDistance))
+        {
+            AdjustDirectionToAvoidWall();
+            return;
+        }
+
+        Vector2 leftDirection = RotateVector(movementDirection, -sideRayAngle);
+        if (CastRay(currentPosition, leftDirection, distanceAvoid))
+        {
+            AdjustDirectionToAvoidWall();
+            return;
+        }
+
+        Vector2 rightDirection = RotateVector(movementDirection, sideRayAngle);
+        if (CastRay(currentPosition, rightDirection, distanceAvoid))
+        {
+            AdjustDirectionToAvoidWall();
+            return;
+        }
+    }
+
+    private void AdjustDirectionToAvoidWall()
+    {
+        float randomAngle = Random.Range(-90f, 90f);
+        movementDirection = RotateVector(movementDirection, randomAngle).normalized;
+        Debug.Log($"Adjusting direction to avoid wall: New Direction = {movementDirection}");
+    }
+
+    private bool CastRay(Vector2 origin, Vector2 direction, float distance)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(origin, direction, distance);
+        if (hit.collider != null && hit.collider.CompareTag("Wall"))
+        {
+            Debug.DrawLine(origin, origin + direction * distance, Color.red);
+            return true;
+        }
+        Debug.DrawLine(origin, origin + direction * distance, Color.green);
+        return false;
+    }
+
+    private Vector2 RotateVector(Vector2 vector, float angle)
+    {
+        float radians = angle * Mathf.Deg2Rad;
+        float cos = Mathf.Cos(radians);
+        float sin = Mathf.Sin(radians);
+        return new Vector2(
+            vector.x * cos - vector.y * sin,
+            vector.x * sin + vector.y * cos
+        );
+    }
+
+    private void AvoidOtherRabbits()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, distanceAvoid);
+        foreach (Collider2D hit in hits)
+        {
+            if (hit.gameObject == this.gameObject) continue;
+
+            if (hit.CompareTag("Rabbit"))
+            {
+                Vector2 otherRabbitPosition = hit.transform.position;
+                Vector2 selfPosition = transform.position;
+
+                Vector2 avoidVector = (selfPosition - otherRabbitPosition).normalized;
+                movementDirection = (movementDirection + avoidVector).normalized;
+
+                Debug.Log("Avoiding other rabbit...");
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (Application.isPlaying)
+        {
+            Vector2 currentPosition = transform.position;
+
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawRay(currentPosition, movementDirection * wallDetectionDistance);
+
+            Vector2 leftDirection = RotateVector(movementDirection, -sideRayAngle);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawRay(currentPosition, leftDirection * distanceAvoid);
+
+            Vector2 rightDirection = RotateVector(movementDirection, sideRayAngle);
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawRay(currentPosition, rightDirection * distanceAvoid);
+
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireSphere(currentPosition, distanceAvoid);
         }
     }
 }
