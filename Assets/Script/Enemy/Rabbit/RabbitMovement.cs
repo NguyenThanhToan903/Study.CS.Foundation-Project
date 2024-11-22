60e0b6cf11f8ee7cad7be357ca9cab3d48721f51
ThanhToan <codehub.personal@gmail.com>
2024-11-22 22:21:44 +0700

    update reset old moverabbit

--- Assets/Script/Enemy/Rabbit/RabbitMovement.cs
+++ Assets/Script/Enemy/Rabbit/RabbitMovement.cs
@@ -1,133 +1,67 @@
 ﻿using UnityEngine;
-
-public class RabbitMovement : MonoBehaviour
+//old
+public class Racket : MonoBehaviour
 {
-    [SerializeField] private float speed = 2f;
-    [SerializeField] private float distanceAvoid = 1f; // Bán kính phát hiện thỏ khác
-    [SerializeField] private float distancePlayerAvoid = 2f; // Bán kính phát hiện thỏ khác
-    [SerializeField] private float wallDetectionDistance = 3f;
-    [SerializeField] private float sideRayAngle = 40f; // Góc lệch của hai tia bên
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private ActiveRacket activeRacket;
+    [SerializeField] private Vector3 move;
+    [SerializeField] private Transform racketCollider;
+    [SerializeField] private GameObject pauseGameMenu;
+    private Animator animator;
 
-    private Vector2 movementDirection;
-    private float waitTimer = 0f;
-    private float waitDuration = 0f;
-    private bool isWaiting = false;
-    private bool wallDetected = false;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        playerInput = new PlayerInput();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
 
     private void Start()
     {
-        SetRandomDirection();
+        playerInput.Combat.Catch.started += _ => Attack();
     }
 
     private void Update()
     {
-        // Di chuyển thỏ
-        this.transform.position += (Vector3)(speed * Time.deltaTime * movementDirection);
-
-        if (isWaiting)
-        {
-            // Tăng bộ đếm thời gian khi đang chờ đổi hướng
-            waitTimer += Time.deltaTime;
-
-            if (waitTimer >= waitDuration)
-            {
-                // Khi hết thời gian chờ, đổi hướng và reset cờ
-                isWaiting = false;
-                waitTimer = 0f;
-                wallDetected = false;
-
-                Debug.Log("Changing direction after waiting...");
-                SetRandomDirection();
-            }
-        }
-        else
-        {
-            // Kiểm tra va chạm với tường nếu không trong thời gian chờ
-            CheckWallCollision();
-
-            // Kiểm tra va chạm với thỏ khác
-            CheckRabbitCollision();
-
-            //
-            //CheckPlayerCollision();
-        }
+        MouseFollowWithOffset();
+        GetInput();
     }
 
-    private void SetRandomDirection()
+    private void GetInput()
     {
-        float randomAngle = Random.Range(0f, 360f);
-        movementDirection = new Vector2(Mathf.Cos(randomAngle), Mathf.Sin(randomAngle)).normalized;
+        move = InputManager.Instance.Input.normalized;
     }
 
-    private void CheckWallCollision()
+    private void Attack()
     {
-        Vector2 currentPosition = transform.position;
-
-        if (CastRay(currentPosition, movementDirection, wallDetectionDistance))
-        {
-            StartWaiting();
-            return;
-        }
-
-        // Kiểm tra va chạm tia bên trái
-        Vector2 leftDirection = RotateVector(movementDirection, -sideRayAngle);
-        if (CastRay(currentPosition, leftDirection, distanceAvoid))
-        {
-            StartWaiting();
-            return;
-        }
-
-        // Kiểm tra va chạm tia bên phải
-        Vector2 rightDirection = RotateVector(movementDirection, sideRayAngle);
-        if (CastRay(currentPosition, rightDirection, distanceAvoid))
-        {
-            StartWaiting();
-            return;
-        }
+        animator.SetTrigger("Catching");
+        racketCollider.gameObject.SetActive(true);
     }
 
-
-            if (Mathf.Abs(currentAngle) > halfAngle) // Tia ở cạnh
-            {
-                adjustedDistance = Mathf.Lerp(wallDetectionDistance, distanceAvoid, Mathf.Abs(currentAngle) / halfAngle);
-            }
-
-            RaycastHit2D hit = Physics2D.Raycast(currentPosition, direction, adjustedDistance);
-
-            if (hit.collider != null && hit.collider.CompareTag("Wall"))
-            {
-                movementDirection = -movementDirection;
-                break;
-
-            }
-        }
-    }
-
-
-
-    private void OnDrawGizmos()
+    private void DoneAttackEvent()
     {
-        if (Application.isPlaying)
+        racketCollider.gameObject.SetActive(false);
+    }
+
+    private void MouseFollowWithOffset()
+    {
+        if (Time.timeScale == 1f)
         {
-            Vector2 currentPosition = transform.position;
+            Vector3 mousePos = Input.mousePosition;
+            float temp = 0f;
+            Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerMovement.transform.position);
 
-            // Tia giữa
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawRay(currentPosition, movementDirection * wallDetectionDistance);
+            float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
 
-            // Tia bên trái
-            Vector2 leftDirection = RotateVector(movementDirection, -sideRayAngle);
-            Gizmos.color = Color.blue;
-            Gizmos.DrawRay(currentPosition, leftDirection * distanceAvoid);
-
-            // Tia bên phải
-            Vector2 rightDirection = RotateVector(movementDirection, sideRayAngle);
-            Gizmos.color = Color.cyan;
-            Gizmos.DrawRay(currentPosition, rightDirection * distanceAvoid);
-
-            // Vẽ bán kính va chạm với thỏ khác
-            Gizmos.color = Color.magenta;
-            Gizmos.DrawWireSphere(currentPosition, distanceAvoid);
+            if (move.x < 0) temp = -180f;
+            else temp = 0f;
+            activeRacket.transform.rotation = Quaternion.Euler(0f, temp, angle);
+            racketCollider.transform.rotation = Quaternion.Euler(0f, temp, angle);
         }
     }
 }
